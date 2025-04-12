ca12dd9dc8d5ca53e1dbe56e74c4ca9b565d0d11
John Leo Bruno <brunojohn0004@gmail.com>
2025-04-13 02:52:57 +0800

    add data
    updated data and ready to fetch

---
+++ src/data/data.js
@@ -0,0 +1,218 @@
+const hero = [
+  {
+    id: 1,
+    title: "Today's special",
+    subtitle: "Fresh food menu",
+    paragraph:
+      "Induldge in the extraordinary with our Unforgettable Chicken Bite",
+    time: "Open everyday from 8AM - 11PM",
+    imageURL: "menu/chicken2.png",
+    backgroundImage: "background.png",
+  },
+];
+
+const specialties = [
+  {
+    id: 1,
+    imageURL: "/menu/chicken1.png",
+    price: "P399.00",
+    subtitle: "Roasted Chicken with Carrots",
+  },
+  {
+    id: 2,
+    imageURL: "/menu/pizza2.png",
+    price: "P699.00",
+    subtitle: "Margherita Pizza",
+  },
+  {
+    id: 3,
+    imageURL: "/menu/pizza1.png",
+    price: "P800.00",
+    subtitle: "Pizza Bites",
+  },
+  {
+    id: 4,
+    imageURL: "/menu/chicken1.png",
+    price: "P500.00",
+    subtitle: "Chicken made with Rock",
+  },
+];
+
+const services = [
+  {
+    id: 1,
+    imageURL: "/svg/Order.png",
+    subtitle: "Easy To Order",
+  },
+  {
+    id: 2,
+    imageURL: "/svg/OnTheWay.png",
+    subtitle: "Fastest Delivery",
+  },
+  {
+    id: 3,
+    imageURL: "/svg/Delivered.png",
+    subtitle: "Best Quality",
+  },
+];
+
+const discount = [
+  {
+    id: 1,
+    imageURL: "/menu/discount.png",
+    title: "Make order now and get 10% discount!",
+    paragraph:
+      "Discover the best dishes from across the country, each representing the unique flavors and culinary heritage of our nation. From comforting classics to exquisite specialties, these top food picks showcase the delicious diversity of our local cuisine.",
+  },
+];
+
+const about = [
+  {
+    id: 1,
+    title: "Top Food Picks Across the Country",
+    paragraph:
+      "Discover the best dishes from across the country, each representing the unique flavors and culinary heritage of our nation. From comforting classics to exquisite specialties, these top food picks showcase the delicious diversity of our local cuisine.",
+    imageURL: "/menu/pasta2.png",
+  },
+];
+
+const reviews = [
+  {
+    id: 1,
+    name: "Emily Thompson",
+    subtitle: "⭐⭐⭐⭐",
+    paragraph:
+      "So juicy and flavorful! The marinade had a nice mix of herbs and spices, and the chicken was grilled to perfection. Loved the smoky char — super satisfying meal!",
+    imageURL: "/reviews/person1.jpg",
+  },
+  {
+    id: 2,
+    name: "David Scathieber",
+    subtitle: "⭐⭐⭐⭐⭐",
+    paragraph:
+      "Crispy base, loads of toppings, and perfectly melted cheese — every bite was delicious. You can tell the ingredients are fresh. Totally hit the spot!",
+    imageURL: "/reviews/person2.jpg",
+  },
+  {
+    id: 3,
+    name: "William Anderson",
+    subtitle: "⭐⭐⭐⭐⭐",
+    paragraph:
+      "Crispy on the outside, tender inside — that chicken patty was amazing! The bun was soft and held together well. Super filling and full of flavor.",
+    imageURL: "/reviews/person3.jpg",
+  },
+  {
+    id: 4,
+    name: "Sophia Rodriguez",
+    subtitle: "⭐⭐⭐⭐",
+    paragraph:
+      "Thin crust, cheesy, and the spice level was just right. Pepperoni was generous and had that perfect crisp on the edges. Would order again without thinking twice.",
+    imageURL: "/reviews/person4.jpg",
+  },
+  {
+    id: 5,
+    name: "Michael Anderson",
+    subtitle: "⭐⭐⭐⭐⭐",
+    paragraph:
+      "Crispy, juicy, and not greasy at all! The seasoning was addictive, and the portion size was perfect for sharing. Everyone at the table loved it.",
+    imageURL: "/reviews/person5.jpg",
+  },
+];
+
+const dishes = [
+  {
+    id: 1,
+    price: "P500",
+    title: "Lemon Herb Roasted Chicken",
+    imageURL: "/menu/chicken1.png",
+  },
+  {
+    id: 2,
+    price: "P500",
+    title: "Roasted Chicken with Carrots",
+    imageURL: "/menu/chicken2.png",
+  },
+  {
+    id: 3,
+    price: "P500",
+    title: "Roast Bites",
+    imageURL: "/menu/chicken6.png",
+  },
+  {
+    id: 4,
+    price: "P500",
+    title: "Lemon Herb Chicken",
+    imageURL: "/menu/chicken3.png",
+  },
+  {
+    id: 5,
+    price: "P500",
+    title: "Roasted Chicken Legs",
+    imageURL: "/menu/chicken5.png",
+  },
+  {
+    id: 6,
+    price: "P500",
+    title: "Pizza Bites",
+    imageURL: "/menu/pizza1.png",
+  },
+  {
+    id: 7,
+    price: "P500",
+    title: "Strawchoco Pizza",
+    imageURL: "/menu/pizza3.png",
+  },
+  {
+    id: 8,
+    price: "P500",
+    title: "Margherita Pizza",
+    imageURL: "/menu/pizza2.png",
+  },
+  {
+    id: 9,
+    price: "P500",
+    title: "Burger Bite King",
+    imageURL: "/menu/burger1.png",
+  },
+  {
+    id: 10,
+    price: "P500",
+    title: "Burger Overload",
+    imageURL: "/menu/burger2.png",
+  },
+  {
+    id: 11,
+    price: "P500",
+    title: "Spaghetti Bolognese",
+    imageURL: "/menu/pasta.png",
+  },
+  {
+    id: 12,
+    price: "P500",
+    title: "Meatballs Pasta",
+    imageURL: "/menu/meatballs.png",
+  },
+];
+
+const contact = [
+  {
+    id: 1,
+    title: "Get in touch, let's talk.",
+    subtitle:
+      "For any inquiries, collaboration opportunities, or simply to say hello, feel free to reach out to us through the following contact information.",
+    location: "Nueva Ecija, Philippines",
+    phoneNumber: "+63 992-718-0980",
+    email: "JanggoDev@gmail.com",
+  },
+];
+
+module.exports = {
+  hero,
+  specialties,
+  services,
+  discount,
+  about,
+  reviews,
+  dishes,
+  contact,
+};
